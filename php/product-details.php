2676145bf3eba4dc7b33035bdd63dfca52360d4c
Huynhkha28 <20521426@gm.uit.edu.vn>
2022-11-01 18:51:52 +0700

    add_file_admin

--- php/product-details.php
+++ php/product-details.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../assets/css/style.css">
-    <link rel="stylesheet" href="../../assets/css/base.css">
-    <link rel="stylesheet" href="../../assets/css/details.css">
-    <link rel="stylesheet" href="../../assets/font/fontawesome-free-6.1.2-web/css/all.min.css">
-    <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/base.css">
+    <link rel="stylesheet" href="../assets/css/details.css">
+    <link rel="stylesheet" href="../assets/font/fontawesome-free-6.1.2-web/css/all.min.css">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap-grid.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/js/bootstrap.min.js">
     <script src="../../js/index.js"></script>
     <title>Chi tiết khóa học</title>
