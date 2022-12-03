d60f46304f8ab9a8c32838a0f60a8f5a4733b4e1
lequocdung01 <108891950+lequocdung01@users.noreply.github.com>
2022-12-03 15:42:34 +0700

    quocdung

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
   $("Post_item_info_title").click(function () {
+<<<<<<< Updated upstream
     
     $(this).parent().toggleClass('active');
     $(this).parent().children('.Post_item_info_body').toggleClass();
+=======
+    $(this).parent().toggleClass('active');
+>>>>>>> Stashed changes
   });
 });
